acbeb31c5262d82ffd2d9e2a28ec4ee0f5a90186
gdg6776 <gdg6776@users.noreply.github.com>
2017-10-30 14:50:29 -0400

    Common changes

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 These are the tools for studying how youtube is used to provide and seek support for suicide.
 This project requires Python 2.7 and some data science libraries.
 
-To start the program simply run "python Graph.py" and follow the on screen instructions.
+To start the program simply run the schell script job.sh using following command -
+ - "chmod u+x job.sh" #This makes script runnable for you
+ - "./job.sh"
+
+and follow the on screen instructions.
 This should give you the analysis of Youtube data.
--- createhist.py
+++ createhist.py
@@ -28,18 +28,25 @@ class createhistogram(object):
 
 
         if title == 'Histogram for Coefficients ATRISK' or title == 'Histogram for Coefficients NOTATRISK':
-            plt.hist(newDict.keys(), bins=20, weights=newDict.values())
-            plt.xlim(0, 2)
-            plt.ylim(0, 100)
-            plt.xlabel(x)
-            plt.ylabel("Frequency")
+            # plt.hist(newDict.keys(), bins=20, weights=newDict.values())
+            # plt.xlim(0, 2)
+            # plt.ylim(0, 100)
+            # plt.xlabel(x)
+            # plt.ylabel("Frequency")
+
+            plt.hist(dict_1.values(),normed=True, cumulative=True)
+            # plt.hist(dict_1.keys(), bins=20, weights=dict_1.values(), density = True)
             plt.title(title)
+
         else:
-            plt.hist(newDict.keys(), weights=[(float(value) / total) * 100 for value in newDict.values()],
-                     bins=range(len(newDict.keys())))
-            plt.xlim(0, 10)
-            plt.ylim(0, 100)
-            plt.xlabel(x)
-            plt.ylabel(y)
+            # plt.hist(newDict.keys(), weights=[(float(value) / total) * 100 for value in newDict.values()],
+            #          bins=range(len(newDict.keys())))
+            # plt.xlim(0, 10)
+            # plt.ylim(0, 100)
+            # plt.xlabel(x)
+            # plt.ylabel(y)
+            plt.hist(dict_1.values(),normed=True, cumulative=True)
+            # plt.hist(dict_1.keys(), bins=20, weights=dict_1.values(), density=True)
             plt.title(title)
-        plt.show()
+        # plt.show()
+        plt.savefig(title)
